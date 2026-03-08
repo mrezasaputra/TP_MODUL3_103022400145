24ff291cc8f5c6402a9f79873e05a6ccdf9f9864
mrezasaputra <mrezagithub@gmail.com>
2026-03-09 03:26:58 +0700

    Menambahkan implementasi kalkulator

--- TP_MODUL3_103022400145/Form1.Designer.cs
+++ TP_MODUL3_103022400145/Form1.Designer.cs
@@ -37,7 +37,7 @@
             button7 = new Button();
             button8 = new Button();
             button9 = new Button();
-            button10 = new Button();
+            button0 = new Button();
             buttonTambah = new Button();
             buttonSamaDengan = new Button();
             labelHasil = new Label();
@@ -63,6 +63,7 @@
             button2.TabIndex = 1;
             button2.Text = "2";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
@@ -73,6 +74,7 @@
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
@@ -83,6 +85,7 @@
             button4.TabIndex = 3;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
@@ -93,6 +96,7 @@
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
@@ -103,6 +107,7 @@
             button6.TabIndex = 5;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // button7
             // 
@@ -113,6 +118,7 @@
             button7.TabIndex = 6;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // button8
             // 
@@ -123,6 +129,7 @@
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
@@ -133,16 +140,18 @@
             button9.TabIndex = 8;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
             // 
-            // button10
+            // button0
             // 
-            button10.Font = new Font("Segoe UI", 18F);
-            button10.Location = new Point(273, 457);
-            button10.Name = "button10";
-            button10.Size = new Size(101, 98);
-            button10.TabIndex = 9;
-            button10.Text = "0";
-            button10.UseVisualStyleBackColor = true;
+            button0.Font = new Font("Segoe UI", 18F);
+            button0.Location = new Point(273, 457);
+            button0.Name = "button0";
+            button0.Size = new Size(101, 98);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button10_Click;
             // 
             // buttonTambah
             // 
@@ -153,6 +162,7 @@
             buttonTambah.TabIndex = 10;
             buttonTambah.Text = "+";
             buttonTambah.UseVisualStyleBackColor = true;
+            buttonTambah.Click += buttonTambah_Click;
             // 
             // buttonSamaDengan
             // 
@@ -163,6 +173,7 @@
             buttonSamaDengan.TabIndex = 11;
             buttonSamaDengan.Text = "=";
             buttonSamaDengan.UseVisualStyleBackColor = true;
+            buttonSamaDengan.Click += buttonSamaDengan_Click;
             // 
             // labelHasil
             // 
@@ -181,7 +192,7 @@
             Controls.Add(labelHasil);
             Controls.Add(buttonSamaDengan);
             Controls.Add(buttonTambah);
-            Controls.Add(button10);
+            Controls.Add(button0);
             Controls.Add(button9);
             Controls.Add(button8);
             Controls.Add(button7);
@@ -207,7 +218,7 @@
         private Button button7;
         private Button button8;
         private Button button9;
-        private Button button10;
+        private Button button0;
         private Button buttonTambah;
         private Button buttonSamaDengan;
         private Label labelHasil;
--- TP_MODUL3_103022400145/Form1.cs
+++ TP_MODUL3_103022400145/Form1.cs
@@ -2,6 +2,8 @@ namespace TP_MODUL3_103022400145
 {
     public partial class Form1 : Form
     {
+        int totalHasil = 0;
+        string layarKetik = "";
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +11,83 @@ namespace TP_MODUL3_103022400145
 
         private void button1_Click(object sender, EventArgs e)
         {
+            layarKetik += "1";
+            labelHasil.Text = layarKetik;
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            layarKetik += "2";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            layarKetik += "3";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            layarKetik += "4";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            layarKetik += "5";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            layarKetik += "6";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            layarKetik += "7";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            layarKetik += "8";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            layarKetik += "9";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            layarKetik += "0";
+            labelHasil.Text = layarKetik;
+        }
+
+        private void buttonTambah_Click(object sender, EventArgs e)
+        {
+            if (layarKetik != "")
+            {
+                totalHasil += Convert.ToInt32(layarKetik);
+                layarKetik = "";
+                labelHasil.Text = "";
+            }
+        }
+
+        private void buttonSamaDengan_Click(object sender, EventArgs e)
+        {
+            if (layarKetik != "")
+            {
+                totalHasil += Convert.ToInt32(layarKetik);
+                labelHasil.Text = Convert.ToString(totalHasil);
+                totalHasil = 0;
+                layarKetik = "";
+            }
         }
     }
 }
